f4aa7555c8787871f85fe7661b7e3ebcaff64190
Ankit Pandey <9999385728ankit@gmail.com>
2024-05-19 17:12:40 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,13 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+
+    connection.on('data', (data: string) =>{
+        console.log("Got data from server - ", data)
+
+        connection.write("+PONG\r\n")
+    })
+
 });
 
 server.listen(6379, "127.0.0.1");
